ecefe2d8ccb7c7cb3240593de604751c50122acf
Marcus Vinicius Galhardi Ferreira <magro.ferreira@gmail.com>
2025-04-11 02:55:46 +0000

    adciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-  return <h1>Paula Talita, você é minha rainha, se você já deu o c*, dê uma risadinha</h1>
+function Home() {
+  return (
+    <h1>
+      Paula Talita, você é minha rainha, se você já deu o c*, dê uma risadinha
+    </h1>
+  );
 }
 
-export default Home
+let teste = "teste";
+
+export default Home;
